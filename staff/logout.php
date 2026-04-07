e53196a138d5c52a47b02e51666a3dc6123e497e
DarelSeviola <stefannyseviolapalomares@gmail.com>
2026-04-07 23:49:36 +0800

    Refactor logout process to clear session data

--- staff/logout.php
+++ staff/logout.php
@@ -1,13 +1,19 @@
 <?php
+// Start staff session
 session_name('staff_session');
 session_start();
 
-session_unset();
+// Clear all session data
+$_SESSION = [];
+
+// Destroy session
 session_destroy();
 
+// Delete session cookie
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time() - 3600, '/');
 }
 
+// Redirect to login page
 header("Location: login.php");
 exit();
